030628b56bf53133d044bce9c5aa7f99e67a9af1
Leila Pearson <873990+leilapearson@users.noreply.github.com>
2020-05-03 17:54:48 -0700

    Remove unused predicates.

--- src/predicates/matches.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Returns true if the received string contains the expected string, or if the received string
- * matches the supplied regular expression
- */
-export const matches = (expected: string | RegExp) => (received: string): boolean =>
-  typeof expected === 'string' ? received.includes(expected) : new RegExp(expected).test(received);
